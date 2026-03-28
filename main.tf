982e2ece42c92ac85a768a49e51c6ccbcde28e75
Nik <nikhilkapade29@gmail.com>
2026-03-28 11:17:25 +0530

    Modify EC2 instance configuration in main.tf
    Updated instance type and EC2 tags, removed connection and provisioner blocks.

--- main.tf
+++ main.tf
@@ -1,31 +1,9 @@
 resource "aws_instance" "name" {
   ami = "ami-0edc0a81903bf24ef"
-  instance_type = "c7i-flex.large"
+  instance_type = "t3.micro"
   key_name = "demo1"
   tags = {
-    Name= "jenkinEC2"
-  }
-
-  connection {
-    type        = "ssh"
-    user        = "ec2-user"
-    private_key = file("C:\\Users\\nikhi\\Downloads\\demo1.pem")
-    host        = self.public_ip
-  }
-
-  provisioner "remote-exec" {
-    inline = [ 
-      "sudo yum install git -y",
-      "sudo yum install java -y",
-      "sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo",
-      "sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key",
-      "sudo yum install jenkins -y",
-      "sudo systemctl enable jenkins",
-      "sudo systemctl start jenkins",
-      "sudo yum install -y yum-utils",
-      "sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo",
-      "sudo yum install terraform -y"
-     ]
+    Name= "automated-1"
   }
 }
 
